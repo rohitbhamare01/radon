aff4b55e33af9295649ed0baaefc528493115e37
rohitbhamare01 <mayur.bhamare2941@gmail.com>
2022-06-09 00:25:55 +0530

    assigenmentroute.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,39 @@
 const express = require('express');
-const externalModule = require('./logger')
+
+const lodash = require('lodash');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
+router.get('/hello', function (req, res) {
+    // ====================================1==========================================
+    let month = ["january", "february", "march","april","may","june","july","august","september","ocober","november","december"]
+    const result = lodash.chunk(month, [size=4]);
+    console.log(result);
+    // =====================================2==========================================
+    let number = [1, 3, 5, 7 , 9, 11 , 13, 15, 17, 19]
+    const result2 = lodash.tail(number)
+    console.log(result2);
+
+    // =====================================3=======================================
+    const demo =   [25,4,5,6,7,8]
+    const demo1 =    [43,56,7,23,4]
+    const demo2 =    [65,23,8,12,56]
+    const demo3 =    [90,75,34,29,43]
+    const demo4 =    [13,29,65,97,36]
+        
+    const result3 =lodash.union(demo,demo1,demo2,demo3.demo4)
+    console.log(result3);
+    // =======================================4============================================
+    
+    const Arr = ([["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]])
+
+    const result4 =lodash.fromPairs(Arr)
+    console.log(result4)
+    
+    // externalModule.log()
+    res.send('this is rohit bhamare api')
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
-
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
-
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
 
 module.exports = router;
 // adding this comment for no reason
