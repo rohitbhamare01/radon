6edd89720b0306f84afc40fe2001ca45d72e4ce0
rohitbhamare01 <mayur.bhamare2941@gmail.com>
2022-06-09 01:53:21 +0530

    assigenment players

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,53 @@
 const express = require('express');
-const externalModule = require('./logger')
-
 const router = express.Router();
-
-router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
-});
-
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
-
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
-
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
-
+ 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+ 
+   router.post('/players', function (req, res) {
+       let demo = req.body;
+       let isSame = false;
+       players.forEach(players =>{
+           if (players.name == demo.name){
+               isSame =true;
+           }
+       });
+       if (!isSame){
+           players.push(demo);
+       }
+       
+       console.log(demo);
+ 
+       //LOGIC WILL COME HERE
+       res.send(  { demo: players , status: true }  )
+   })
+  
 module.exports = router;
-// adding this comment for no reason
