a40bdc103c3df4648b474e4f1abbfba561a46f09
rohitbhamare01 <mayur.bhamare2941@gmail.com>
2022-06-21 01:04:40 +0530

    assigenment book for mongodb

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,15 @@
 const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
+const createNewBook= async function (req, res) {
     let data= req.body
     let savedData= await UserModel.create(data)
     res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const getAllBook= async function (req, res) {
+    let allbooks= await UserModel.find()
+    res.send({msg: allbooks})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.createNewBook= createNewBook
+module.exports.getAllBook= getAllBook
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,8 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", UserController.createNewBook  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getBookData", UserController.getAllBook)
 
 module.exports = router;
